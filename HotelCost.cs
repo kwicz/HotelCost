58f16bdd066b1a84bb4c5cf99bcbf64acd977d97
Katy Solovewicz <katy@solovewicz.com>
2020-02-22 22:20:37 -0800

    add file setup with all operators in place

---
+++ HotelCost.cs
@@ -0,0 +1,33 @@
+using System;
+
+class HotelCost
+{
+  static void Main()
+  {
+    Console.WriteLine("In what season are you booking a stay?");
+    string season = Console.ReadLine();
+
+    Console.WriteLine("On the weekend or a weeknight?");
+    string dayOfWeek = Console.ReadLine();
+
+    bool summer = season == "summer";  // summer will be true if the season is "summer"
+    bool weekend = dayOfWeek == "weekend";
+
+    if (summer && weekend)
+    {
+      Console.WriteLine("Your stay is probably going to be pretty expensive. It's both peak travel season AND the weekend.");
+    }
+    else if (summer || weekend)
+    {
+      Console.WriteLine("Your stay might be more expensive than normal!");
+    }
+    else if (!(summer || weekend))
+    {
+      Console.WriteLine("Your stay will likely be the cheapest possible!");
+    }
+    else
+    {
+      Console.WriteLine("Your stay might be expensive, but it could be worse!");
+    }
+  }
+}
